2436e5090a1cca694512a0e1c30a01216abe3b66
Sibusiso <sbudahnkoane1@gmail.com>
2024-03-04 19:22:05 +0200

    Add Models: 1. Role 2. Account

---
+++ CampusQuartersAPI/Models/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace CampusQuartersAPI.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public int RoleId { get; set; }
+    }
+}
