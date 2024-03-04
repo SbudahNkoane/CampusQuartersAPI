616d983e939c985279a08f3a8143287eb130d064
Sibusiso <sbudahnkoane1@gmail.com>
2024-03-04 22:30:12 +0200

    Add Get endpoints

---
+++ CampusQuartersAPI/Controllers/AccountsController.cs
@@ -0,0 +1,38 @@
+﻿using CampusQuartersAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CampusQuartersAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly CampusQuartersDataContext _context;
+
+        public AccountsController(CampusQuartersDataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAccounts() 
+        {
+            var accounts = _context.Account.ToList();
+           
+            return Ok(accounts);
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetAccount(int id) 
+        {
+            var account = _context.Account.FirstOrDefault(x => x.Id == id);
+            if(account == null)
+            {
+                return NotFound("Account does not exist");
+            }
+            return Ok(account);
+        }
+    }
+}
---
+++ CampusQuartersAPI/Controllers/RolesController.cs
@@ -0,0 +1,40 @@
+﻿using CampusQuartersAPI.Data;
+using CampusQuartersAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CampusQuartersAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly CampusQuartersDataContext _context;
+
+        public RolesController(CampusQuartersDataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetRoles() 
+        {
+            var roles = _context.Roles.ToList();
+
+            
+            return Ok(roles);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetRole(int id)
+        {
+            var role = _context.Roles.FirstOrDefault(i => i.Id==id);
+            if(role == null)
+            {
+                return NotFound("This Role does not exist");
+            }
+            return Ok(role);
+        }
+    }
+}
