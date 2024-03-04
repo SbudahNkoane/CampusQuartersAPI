e3d99994bb064d7ac71ebe73f37ad10d747f4754
Sibusiso <sbudahnkoane1@gmail.com>
2024-03-04 19:12:18 +0200

    Add Data Context class

---
+++ CampusQuartersAPI/Data/CampusQuartersDataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CampusQuartersAPI.Data
+{
+    public class CampusQuartersDataContext:DbContext
+    {
+        public CampusQuartersDataContext(DbContextOptions<CampusQuartersDataContext> options):base(options) 
+        {
+            
+        }
+    }
+}
