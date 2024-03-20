b7f770a607fdf2a2de798031d9de1bdebb6d131e
Sibusiso <sbudahnkoane1@gmail.com>
2024-03-20 10:03:33 +0200

    Add User abstract class

---
+++ CampusQuartersAPI/User.cs
@@ -0,0 +1,16 @@
+﻿using CampusQuartersAPI.Models;
+using System.Reflection.Metadata.Ecma335;
+
+namespace CampusQuartersAPI
+{
+    public abstract class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string EmailAddress { get; set; }
+        public int CellNumber { get; set; }
+        public Account Account { get; set; }
+
+    }
+}
