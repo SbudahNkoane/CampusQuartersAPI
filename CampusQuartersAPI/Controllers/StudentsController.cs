9951da58e5842d70b0bb1f233e0f63d91b13f1d4
Sibusiso <sbudahnkoane1@gmail.com>
2024-03-20 11:29:51 +0200

    Add Students Controller with Get,Post and Delete methods

---
+++ CampusQuartersAPI/Controllers/StudentsController.cs
@@ -0,0 +1,64 @@
+﻿using CampusQuartersAPI.Data;
+using CampusQuartersAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CampusQuartersAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly CampusQuartersDataContext _context;
+
+        public StudentsController(CampusQuartersDataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            var students = _context.Students.ToList();
+
+            return Ok(students);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            var student = _context.Students.FirstOrDefault(x => x.Id == id);
+            if (student == null)
+            {
+                return NotFound("This student does not exist");
+            }
+            return Ok(student);
+        }
+
+        [HttpPost]
+
+        public IActionResult PostStudent([FromBody]Student student)
+        {
+             student.Account.RoleId = 1;
+            _context.Students.Add(student);
+            
+            _context.SaveChanges();
+            return Ok(student);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult DeleteStudent(int id) 
+        {
+            var student = _context.Students.FirstOrDefault(_x => _x.Id == id);
+            if(student == null)
+            {
+                return BadRequest("Student does not exist");
+            }
+            _context.Students.Remove(student); 
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
