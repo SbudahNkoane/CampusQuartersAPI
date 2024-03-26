dfbd03381f073470bb9e8fa047004ea0d5b5c51d
Sibusiso <sbudahnkoane1@gmail.com>
2024-03-26 21:19:10 +0200

    Add AccommodationImage Model

---
+++ CampusQuartersAPI/Models/AccommodationImage.cs
@@ -0,0 +1,12 @@
+﻿namespace CampusQuartersAPI.Models
+{
+    public class AccommodationImage
+    {
+        public int Id { get; set; }
+        public string ImageURL { get; set; }    
+        public DateTime DateUploaded { get; set; }
+        public DateTime DateTaken { get; set; }
+        public Photographer Photographer { get; set; } 
+
+    }
+}
