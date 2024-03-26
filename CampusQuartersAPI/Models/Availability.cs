da7f615728029c8488eb577f0fc5e1622ae05a42
Sibusiso <sbudahnkoane1@gmail.com>
2024-03-26 19:59:56 +0200

    Add Availability model

---
+++ CampusQuartersAPI/Models/Availability.cs
@@ -0,0 +1,10 @@
+﻿namespace CampusQuartersAPI.Models
+{
+    public class Availability
+    {
+        public int Id { get; set; }
+        public bool IsAvailable { get; set; }
+        public AvailabilityStatus Status { get; set; }
+        public DateTime DateAvailable { get; set; } 
+    }
+}
