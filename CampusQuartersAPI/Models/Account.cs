84ca7031ac0119966891e980d00d2ab17c7425c8
Sibusiso <sbudahnkoane1@gmail.com>
2024-03-26 17:41:48 +0200

    remove email field

--- CampusQuartersAPI/Models/Account.cs
+++ CampusQuartersAPI/Models/Account.cs
@@ -3,7 +3,6 @@
     public class Account
     {
         public int Id { get; set; }
-        public string EmailAddress { get; set; }
         public string Password { get; set; }
         public int RoleId { get; set; }
     }
