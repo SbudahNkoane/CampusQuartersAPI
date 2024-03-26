8ff583f910eefeafc26b13c92fc540dbd490886b
Sibusiso <sbudahnkoane1@gmail.com>
2024-03-26 21:58:50 +0200

    Add Bookings Migration

---
+++ CampusQuartersAPI/Migrations/20240326195646_BookingsMigration.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CampusQuartersAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class BookingsMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    AccommodationId = table.Column<int>(type: "int", nullable: false),
+                    BookedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsTaken = table.Column<bool>(type: "bit", nullable: false),
+                    IsViewed = table.Column<bool>(type: "bit", nullable: false),
+                    ViewDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Bookings_Accommodations_AccommodationId",
+                        column: x => x.AccommodationId,
+                        principalTable: "Accommodations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Bookings_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_AccommodationId",
+                table: "Bookings",
+                column: "AccommodationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_StudentId",
+                table: "Bookings",
+                column: "StudentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bookings");
+        }
+    }
+}
