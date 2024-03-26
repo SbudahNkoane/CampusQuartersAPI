8fcf0a39ede363a7a6582b7856eb525ac1e7233c
Sibusiso <sbudahnkoane1@gmail.com>
2024-03-26 18:44:01 +0200

    Add Landlords migration

---
+++ CampusQuartersAPI/Migrations/20240326160321_LandlordsMigration.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CampusQuartersAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class LandlordsMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailAddress",
+                table: "Account");
+
+            migrationBuilder.CreateTable(
+                name: "Landlords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmailAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CellNumber = table.Column<int>(type: "int", nullable: false),
+                    AccountId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Landlords", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Landlords_Account_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Account",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Landlords_AccountId",
+                table: "Landlords",
+                column: "AccountId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Landlords");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmailAddress",
+                table: "Account",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
