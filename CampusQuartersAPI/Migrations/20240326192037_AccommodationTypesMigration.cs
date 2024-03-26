b6fad2e92848d9938e695982a8bf662aa4c9fd91
Sibusiso <sbudahnkoane1@gmail.com>
2024-03-26 21:25:21 +0200

    Add Accommodation Types Migration

---
+++ CampusQuartersAPI/Migrations/20240326192037_AccommodationTypesMigration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CampusQuartersAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AccommodationTypesMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AccommodationTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccommodationTypes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AccommodationTypes");
+        }
+    }
+}
