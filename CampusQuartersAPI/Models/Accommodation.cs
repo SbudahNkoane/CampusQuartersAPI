0aecb10409e1c35c99f43879acd4f30cea76eb98
Sibusiso <sbudahnkoane1@gmail.com>
2024-03-26 21:43:33 +0200

    Add Accommodation Model

---
+++ CampusQuartersAPI/Models/Accommodation.cs
@@ -0,0 +1,19 @@
+﻿namespace CampusQuartersAPI.Models
+{
+    public class Accommodation
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public List<AccommodationImage> Images { get; set; }
+        public List<AccommodationVideo> Videos { get; set; }
+        public AccommodationType Type { get; set; }
+        public Availability Availability { get; set; }
+        public Landlord Landlord { get; set; }
+        public DateTime Photographed { get; set; }
+        public DateTime Created { get; set; }
+        public string Description { get; set; }
+        public double Deposit { get; set; }
+        public double Rent { get; set; }
+        public string Name { get; set; }    
+    }
+}
