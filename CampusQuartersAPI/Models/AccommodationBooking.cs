48e5ee8774eb28442bb46d283b48f57409b73d50
Sibusiso <sbudahnkoane1@gmail.com>
2024-03-26 21:55:59 +0200

    Add Booking Model

---
+++ CampusQuartersAPI/Models/AccommodationBooking.cs
@@ -0,0 +1,13 @@
+﻿namespace CampusQuartersAPI.Models
+{
+    public class AccommodationBooking
+    {
+        public int Id { get; set; }
+        public Student Student { get; set; }
+        public Accommodation Accommodation { get; set; }
+        public DateTime BookedAt { get; set; }
+        public bool IsTaken { get; set; } = false;
+        public bool IsViewed { get; set; }=false;
+        public DateTime? ViewDate { get; set; }
+    }
+}
