babcc8a4234338e694d22312206faba6b96c8e6c
Sibusiso <sbudahnkoane1@gmail.com>
2024-03-26 21:55:48 +0200

    Make other fields nullable

--- CampusQuartersAPI/Models/Accommodation.cs
+++ CampusQuartersAPI/Models/Accommodation.cs
@@ -4,12 +4,12 @@
     {
         public int Id { get; set; }
         public string Address { get; set; }
-        public List<AccommodationImage> Images { get; set; }
-        public List<AccommodationVideo> Videos { get; set; }
+        public List<AccommodationImage>? Images { get; set; }
+        public List<AccommodationVideo>? Videos { get; set; }
         public AccommodationType Type { get; set; }
         public Availability Availability { get; set; }
         public Landlord Landlord { get; set; }
-        public DateTime Photographed { get; set; }
+        public DateTime? Photographed { get; set; }
         public DateTime Created { get; set; }
         public string Description { get; set; }
         public double Deposit { get; set; }
