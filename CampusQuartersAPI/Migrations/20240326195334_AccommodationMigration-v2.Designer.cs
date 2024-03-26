05354bf0e7bd4276edbc3072c884f21925398f3c
Sibusiso <sbudahnkoane1@gmail.com>
2024-03-26 21:55:16 +0200

    fix Accommodation Migration

--- CampusQuartersAPI/Migrations/20240326195334_AccommodationMigration-v2.Designer.cs
+++ CampusQuartersAPI/Migrations/20240326195334_AccommodationMigration-v2.Designer.cs
@@ -4,6 +4,7 @@ using CampusQuartersAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CampusQuartersAPI.Migrations
 {
     [DbContext(typeof(CampusQuartersDataContext))]
-    partial class CampusQuartersDataContextModelSnapshot : ModelSnapshot
+    [Migration("20240326195334_AccommodationMigration-v2")]
+    partial class AccommodationMigrationv2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
