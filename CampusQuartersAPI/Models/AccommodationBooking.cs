2200df0930b6ade7767ee2284a0c3e2454ca9368
Sibusiso <sbudahnkoane1@gmail.com>
2024-03-26 22:17:46 +0200

    remove student field

--- CampusQuartersAPI/Models/AccommodationBooking.cs
+++ CampusQuartersAPI/Models/AccommodationBooking.cs
@@ -3,7 +3,6 @@
     public class AccommodationBooking
     {
         public int Id { get; set; }
-        public Student Student { get; set; }
         public Accommodation Accommodation { get; set; }
         public DateTime BookedAt { get; set; }
         public bool IsTaken { get; set; } = false;
